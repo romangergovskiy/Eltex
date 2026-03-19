75830349756e6abd8d6cb0e5870725b73e8f7428
Герговский Роман Викторович <ip312s04@sibsutis.ru>
2026-03-20 00:11:06 +0700

    dz 3

---
+++ eltex_hw_3.2/eltex_hw_3.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eltex_hw_3.2
+//
+//  Created by Роман Герговский on 19.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
